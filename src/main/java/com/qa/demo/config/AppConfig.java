637db5b1f8153abfaed819b1112254abec5e1932
QAcpardeep <cpardeep@qa.com>
2021-01-21 11:25:53 +0000

    HWAP-16

--- src/main/java/com/qa/demo/config/AppConfig.java
+++ src/main/java/com/qa/demo/config/AppConfig.java
@@ -1,5 +1,16 @@
 package com.qa.demo.config;
 
+import java.time.LocalTime;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+
+	@Bean
+	public String greeting() {
+		return "Welcome to the HWA project, the time is: " + LocalTime.now().toString();
+	}
 
 }

8e60a9b45e4b7f575ad262c02d83991456e1684e
QAcpardeep <cpardeep@qa.com>
2021-01-25 09:58:22 +0000

    HWAP-6 HWAP-42

--- src/main/java/com/qa/demo/persistence/domain/Shelter.java
+++ src/main/java/com/qa/demo/persistence/domain/Shelter.java
@@ -10,11 +10,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;

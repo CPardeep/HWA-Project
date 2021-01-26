3f8d4b9ac1781546cb6ad01e9297ecedebc5d2c9
QAcpardeep <cpardeep@qa.com>
2021-01-26 22:05:52 +0000

    HWAP-47

--- src/main/resources/static/Dog.js
+++ src/main/resources/static/Dog.js
@@ -1,5 +1,5 @@
 "use strict";
-const dogId = document.querySelector("#dogName");
+const dogId = document.querySelector("#dogId");
 const dogName = document.querySelector("#dogName");
 const dogBreed = document.querySelector("#dogBreed");
 const dogAge = document.querySelector("#dogAge");
@@ -26,3 +26,23 @@ const createDog = () => {
         .catch((err => console.error("Error please stop what you're doing")))
 }
 
+const updateDog = () => {
+
+    fetch("http://localhost:8080/dog/update/"+ dogId.value, {
+        method: 'PUT',
+        body: JSON.stringify({
+            "name": dogName.value,
+            "breed": dogBreed.value,
+            "age": dogAge.value,
+            "colour": dogColour.value,
+            "available": dogAvailable.value
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+        .catch((err => console.error("Error please stop what you're doing")))
+
+}

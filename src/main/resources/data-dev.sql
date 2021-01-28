080f2dabea9dd0877293fddb1ad25e60a320d24d
QAcpardeep <cpardeep@qa.com>
2021-01-28 16:12:31 +0000

    HWAP-8

---
+++ src/main/resources/data-dev.sql
@@ -0,0 +1,9 @@
+INSERT INTO shelter (address_line, name, postcode) 
+VALUES 
+('73 Bradley Road', 'Chetans Shelter', 'WV22AZ');
+
+INSERT INTO DOG (age, available, breed, colour, name, shelter_ID) 
+VALUES 
+(1, 'YES', 'Akita', 'black', 'rokit', 1),
+(2, 'YES', 'Bulldog', 'black', 'powel', 1);
+
---
+++ src/test/java/com/qa/demo/persistance/UnitTest.java
@@ -0,0 +1,39 @@
+package com.qa.demo.persistance;
+
+import org.junit.jupiter.api.Test;
+
+import com.qa.demo.persistence.domain.Dog;
+import com.qa.demo.persistence.domain.Shelter;
+import com.qa.demo.persistence.dto.DogDTO;
+import com.qa.demo.persistence.dto.ShelterDTO;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+class UnitTest {
+
+//	@Test
+//	public void testShelter() {
+//		EqualsVerifier.simple().forClass(Dog.class)
+//		.withPrefabValues(Shelter.class, new Shelter(), new Shelter(1L, "CBS shell", "73 Bradley Lane", "WV22AZ", null)).verify();
+//	}
+//	
+//	@Test
+//	public void testDog() {
+//		EqualsVerifier.simple().forClass(Dog.class)
+//		.withPrefabValues(Dog.class, new Dog(), new Dog(1L, "rokit", "lebra", 1, "#0000", "yes", null)).verify();
+//	}
+//	
+//	@Test
+//	void testShelterDTO() {
+//		EqualsVerifier.simple().forClass(ShelterDTO.class)
+//		.withPrefabValues(ShelterDTO.class, new ShelterDTO(), new ShelterDTO(1L, "CBS shell", "73 Bradley Lane", "WV22AZ")).verify();
+//	}
+//	
+//	@Test
+//	void testDogDTO() {
+//		EqualsVerifier.simple().forClass(ShelterDTO.class)
+//		.withPrefabValues(DogDTO.class, new DogDTO(), new DogDTO(1L, "rokit", "lebra", 1, "#0000", "yes")).verify();
+//	}
+	
+
+}
